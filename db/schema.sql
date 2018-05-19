0ecd5b0445d0c6d537df87771087196bddea9c0b
sidneyofford <33263806+sidneyofford@users.noreply.github.com>
2018-05-19 07:14:00 -0500

    Add files via upload

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(50) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT 0,
+  date TIMESTAMP NOT NULL,
+  PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chili Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Meat Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Guacamole Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Cheese Burger', true);
